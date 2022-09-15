d75d3ad57cb9f444e9cc14979b7888bee2e73049
Kade Illian <kadeillianmt@gmail.com>
2022-09-15 13:30:48 -0600

    added user authentication minus implementing it in the pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pets#index"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
