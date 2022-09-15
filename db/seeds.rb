e1a17451d98c6d092ba323cc5a5c470c8b0cbed8
Kade Illian <kadeillianmt@gmail.com>
2022-09-15 12:09:57 -0600

    added pets routes using resources, seeded instances of pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Pet.create(
+#   name: "Scout",
+#   breed: "Yellow Lab",
+#   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2QzCUwqxu-hvWcJCn1M24EiIA2TPlnbt-QJKGQ9pq&s",
+# )
+
+# Pet.create(
+#   name: "Maverick",
+#   breed: "A Cat",
+#   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1i5jsUxHORrMBmWUW_85YyiG2SP_C7htZw&usqp=CAU",
+# )
+
+# Pet.create(
+#   name: "Horsie",
+#   breed: "Horse",
+#   image: "https://www.treehugger.com/thmb/SShPLoEHvhEViNtPvs82-QcCPrQ=/2121x1193/smart/filters:no_upscale()/horse.primary-e9a47e1c486c4fb7bf729e05b59cf0df.jpg",
+# )
